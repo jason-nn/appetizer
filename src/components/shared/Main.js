dbe8594b4eabc4424d5de499e6e899f354d4827d
Jason Ho <jason.ho@obf.ateneo.edu>
2022-01-12 22:13:50 +0800

    Change main min height

--- src/components/shared/Main.js
+++ src/components/shared/Main.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Main = styled.div`
   width: 100vw;
-  min-height: calc(100vh - 60px);
+  min-height: calc(100vh - 70px);
   background-color: #f8f9fb;
 
   margin-top: 70px;
