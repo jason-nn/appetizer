43d6f879e35bb080e1e71d8bd7ba6b2981ac1651
Jason Ho <jason.ho@obf.ateneo.edu>
2022-01-15 23:45:13 +0800

    Add purchases data

---
+++ src/data/purchases.js
@@ -0,0 +1,38 @@
+export const purchases = [
+  {
+    id: 1,
+    ticketsBought: 10,
+    purchasedBy: 'jason@parapol.ph',
+    raffle: {
+      title: 'Appetizer Raffle',
+      ticketPrice: 500,
+    },
+  },
+  {
+    id: 2,
+    ticketsBought: 20,
+    purchasedBy: 'jason@parapol.ph',
+    raffle: {
+      title: 'Main Course Raffle',
+      ticketPrice: 400,
+    },
+  },
+  {
+    id: 3,
+    ticketsBought: 10,
+    purchasedBy: 'carl@parapol.ph',
+    raffle: {
+      title: 'Appetizer Raffle',
+      ticketPrice: 500,
+    },
+  },
+  {
+    id: 3,
+    ticketsBought: 5,
+    purchasedBy: 'jason@parapol.ph',
+    raffle: {
+      title: 'Appetizer Raffle',
+      ticketPrice: 500,
+    },
+  },
+];
