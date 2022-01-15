142468704f2822ea413eff99a1f8d4ebd2d69ff9
Jason Ho <jason.ho@obf.ateneo.edu>
2022-01-16 00:14:25 +0800

    Add purchases page

---
+++ src/components/participate/Purchase.js
@@ -0,0 +1,60 @@
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+
+import { Context } from '../../App';
+
+const Container = styled.div`
+  background-color: white;
+  width: 100%;
+  padding: 30px;
+
+  box-shadow: rgba(0 0, 0, 0.08) 0px 4px 12px;
+  border-radius: 8px;
+  margin-bottom: 30px;
+
+  position: relative;
+
+  -webkit-animation: swing-in-right-fwd 0.5s
+    cubic-bezier(0.175, 0.885, 0.32, 1.275) both;
+  animation: swing-in-right-fwd 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275)
+    both;
+
+  position: relative;
+`;
+
+const Title = styled.div`
+  font-family: 'CircularStd-Black', Helvetica, sans-serif;
+  font-size: 25px;
+
+  margin-bottom: 30px;
+`;
+
+const TicketsBought = styled.div`
+  background-image: linear-gradient(35deg, #a83232, #ff5b5b);
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  font-family: 'CircularStd-Black', Helvetica, sans-serif;
+  font-size: 30px;
+`;
+
+const TicketPrice = styled.div`
+  font-family: 'CircularStd-Medium', Helvetica, sans-serif;
+  font-size: 20px;
+
+  color: gray;
+
+  margin-top: 30px;
+`;
+
+export default function Purchase({ title, price, quantity }) {
+  const context = useContext(Context);
+
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <TicketsBought>{quantity} tickets</TicketsBought>
+      <TicketPrice>{context.toCurrency(price)} each</TicketPrice>
+    </Container>
+  );
+}
--- src/components/participate/Purchases.js
+++ src/components/participate/Purchases.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
 
+import Purchase from './Purchase';
+
 import { Context } from '../../App';
 
 const Container = styled.div`
@@ -10,5 +12,22 @@ const Container = styled.div`
 export default function Purchases() {
   const context = useContext(Context);
 
-  return <Container></Container>;
+  return (
+    <Container>
+      {context.purchasesArray
+        .filter((purchase) => {
+          return purchase.purchasedBy === context.currentUser.email;
+        })
+        .map((purchase, key) => {
+          return (
+            <Purchase
+              key={key}
+              title={purchase.raffle.title}
+              price={purchase.raffle.ticketPrice}
+              quantity={purchase.ticketsBought}
+            />
+          );
+        })}
+    </Container>
+  );
 }
